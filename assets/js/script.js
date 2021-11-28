9ed393cc6cf8aa4e0283581a2e912cc7a0d5f8d0
Bubba <bubba.dave.7@gmail.com>
2021-11-27 20:19:49 -0700

    added functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,8 +36,89 @@ $(document).ready(function() {
 
             }
 
-            
+            $columnHour.text(columnHourly);
 
+            let $descriptionColumn = $("<div></div>")
+            .addClass("col-md-9");
+
+            let $descriptionColumnSpan = $("<input></input>")
+            .addClass('description taskDescriptionSpan')
+            .attr('type', 'text')
+            .attr('id',`input-${hourIndex}`)
+            .attr('hour-index', hourIndex);
+
+            
+            $descriptionColumnSpan.val(taskDescrArr[hourIndex]);
+
+
+            let $saveButtonColumn = $("<div></div>")
+            .addClass('col-mad-1 saveBtn')
+
+            let $saveBtnIcon = $("<i></i>")
+            .addClass('fas fa-save btn-save')
+            .attr('id', `saveid-${hourIndex}`)
+            .attr('save-id', hourIndex);
+
+            $rowContainer.append($columnHour);
+
+            $rowContainer.append($descriptionColumn);
+
+            $descriptionColumn.append($descriptionColumnSpan);
+
+            $rowContainer.append($saveButtonColumn);
+
+            $saveButtonColumn.append($saveBtnIcon);
+
+
+
+            $dailyPlannerContainer.append($rowContainer);
+
+        };
+    };
+
+    function getTaskDetails() {
+        let localStorageTasks = JSON.parse(localStorage.getItem("dailyTasks"));
+
+        if (localStorageTasks === null) {
+            taskDescrArr = new Array(9);
         }
-    }
-}
+        else {
+            taskDescrArr = localStorageTasks;
+            console.log("Values from local storage are:" + taskDescrArr);
+        };
+    };
+
+    function taskRowColor () {
+        momentHour = getMomentNow ()
+
+        for (var i = 0; i < 9; i++) {
+            let hourIndex = $("#input-" + i).attr("hour-index");
+            hourIndex = Number(hourIndex);
+            hourIndex +=9;
+
+            if (hourIndex < momentHour) {
+                $("#input-" + i).css("background-color", "lightgrey");
+            } else if (hourIndex > momentHour) {
+                $("#input-" + i).css("background-color","#77dd77");
+            } else {
+                $("#input-" + i).css("background-color","#ff6961");
+            }
+        };
+    };
+
+    $(document).on('click','i',function(event){
+        event.preventDefault();
+
+        let $localStorageIndex = $(this).attr('save-id');
+
+        taskDescrArr[$localStorageIndex] = $("#input-" + $localStorageIndex).val();
+
+        $("#input-" + $localStorageIndex).css("outline: #4CAF50 solid 10px");
+
+        localStorage.setItem("dailyTasks", JSON.stringify(taskDescrArr));
+    });
+
+
+    
+
+});
